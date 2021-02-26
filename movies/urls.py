6dfa0955bec1cedccb2d2f6c18ddf7b28faa2fd3
SadLaboka <toteshephysic@yandex.ru>
2021-02-26 21:39:12 +0300

    Сделал добавление отзывов к фильму

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("movie/", views.MovieListView.as_view()),
     path("movie/<str:slug>/", views.MovieDetailView.as_view()),
+    path("review/", views.ReviewCreateView.as_view()),
 ]
