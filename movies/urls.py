30c57c1e9a9b52f4ab6b29f027bc07bf71d3f4fd
SadLaboka <toteshephysic@yandex.ru>
2021-02-27 20:15:34 +0300

    Сделал рекурсивный вывод ответов на комментарии и добавил возможность добавления рейтинга фильмам

--- movies/urls.py
+++ movies/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("movie/", views.MovieListView.as_view()),
     path("movie/<str:slug>/", views.MovieDetailView.as_view()),
     path("review/", views.ReviewCreateView.as_view()),
+    path("rating/", views.AddStarRatingView.as_view()),
 ]
